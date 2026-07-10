b7fb59a33070ca3a9d5a209f096e36e386761de3
Heloisa Victoria <heloisavictor7@gmail.com>
2026-07-09 22:17:53 -0300

    formatação de strings

---
+++ aula12.py
@@ -0,0 +1,7 @@
+nome = "Aurora Garcia Alencar"
+altura = 1.56
+peso = 49
+imc = peso / altura ** 2
+
+# f-strings
+print(f"{nome} tem {altura:.2f} de altura pesa {peso} quilos e o seu IMC é {imc:.2f}")
---
+++ exercicio2.py
@@ -0,0 +1,6 @@
+nome = "Aurora Garcia Alencar"
+altura = 1.56
+peso = 49
+imc = peso / altura ** 2
+
+print(nome, "tem", altura, "de altura", ", pesa", peso, "quilos e o seu IMC é", imc)
